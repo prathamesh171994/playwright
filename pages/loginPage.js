f96df357a8fb14554aa766a48952a37d4437f296
prathamesh171994@gmail.com <prathamesh171994@gmail.com>
2025-02-04 23:53:26 +0530

    new project

---
+++ pages/loginPage.js
@@ -0,0 +1,25 @@
+// loginPage.js
+
+export class LoginPage {
+    constructor(page) {
+        this.page = page;
+        this.usernameInput = 'input[id="userName"]';
+        this.passwordInput = 'input[id="password"]';
+        this.loginButton = 'button[id="login"]';
+        this.successIndicator = 'text=Dashboard';
+    }
+
+    async goto(url) {
+        await this.page.goto(url);
+    }
+
+    async login(username, password) {
+        await this.page.fill(this.usernameInput, username);
+        await this.page.fill(this.passwordInput, password);
+        await this.page.click(this.loginButton);
+    }
+
+    async isLoginSuccessful() {
+        return await this.page.locator(this.successIndicator).isVisible();
+    }
+}
---
+++ testData.ts
@@ -0,0 +1,16 @@
+export const testData = {
+    urls: [
+        'https://demoqa.com/login',
+        'https://demoqa2.com/login',
+        'https://demoqa3.com/login',
+        'https://demoqa4.com/login',
+        'https://demoqa5.com/login',
+        'https://demoqa6.com/login',
+        'https://demoqa7.com/login',
+        'https://demoqa8.com/login'
+    ],
+    credentials: {
+        username: 'testuser',
+        password: 'testpassword'
+    }
+};
